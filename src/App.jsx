d647fcbe7b977feb5aca5fc56d540eb33c6ec090
soncomqiq <idgamescs@hotmail.com>
2023-05-04 02:31:47 +0700

    Finish lab 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import VideoPlayer from "./components/VideoPlayer";
-import VideoFile from "./assets/1-min-video.mp4"
+import VideoFile from "./assets/1-min-video.mp4";
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [time, setTime] = useState("");
+  const [duration, setDuration] = useState(0);
+
+  const onPlayVideo = () => {
+    setIsPlaying(true);
+    setTime("");
+    setDuration(time);
+  };
+
+  useEffect(() => {
+    console.log("[App.jsx] useEffect");
+    const timerId = setTimeout(() => {
+      console.log("[App.jsx] setTimeout: ", duration);
+      setIsPlaying(false);
+    }, duration * 1000);
+    return () => {
+      console.log("[App.jsx] cleanup function");
+      clearTimeout(timerId);
+    };
+  }, [duration]);
 
   return (
     <>
@@ -14,13 +33,8 @@ function App() {
         value={time}
         onChange={(e) => setTime(e.target.value)}
       />
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? "Pause" : "Play"}
-      </button>
-      <VideoPlayer
-        isPlaying={isPlaying}
-        src={VideoFile}
-      />
+      <button onClick={onPlayVideo}>{isPlaying ? "Pause" : "Play"}</button>
+      <VideoPlayer isPlaying={isPlaying} src={VideoFile} />
     </>
   );
 }
