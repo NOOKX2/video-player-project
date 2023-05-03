93aff5a5e5a63ca13e78a20995a98fd0ac180dcf
soncomqiq <idgamescs@hotmail.com>
2023-05-04 02:19:32 +0700

    Remove logs

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -4,7 +4,6 @@ export function VideoPlayer({ src, isPlaying }) {
   const ref = useRef(null);
 
   useEffect(() => {
-    console.log("useEffect");
     if (isPlaying) {
       ref.current.play();
     } else {
@@ -12,7 +11,6 @@ export function VideoPlayer({ src, isPlaying }) {
     }
   }, [isPlaying]);
 
-  console.log("Render");
   return (
     <div>
       <video ref={ref} src={src} loop playsInline muted />;
