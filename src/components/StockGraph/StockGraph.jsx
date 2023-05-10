1429879b072955751a8e745f90345eaecd2d5201
soncomqiq <idgamescs@hotmail.com>
2023-05-11 01:08:00 +0700

    improve example

--- src/components/StockGraph/StockGraph.jsx
+++ src/components/StockGraph/StockGraph.jsx
@@ -1,10 +1,13 @@
 export function StockGraph() {
+  useEffect(() => {
+    setInterval(() => {
+      console.log("Fetch data");
+    }, 1000);
+  }, []);
+
   return (
     <div>
-      <img
-        alt="stock"
-        src="https://media0.giphy.com/media/JtBZm3Getg3dqxK0zP/200w.webp?cid=ecf05e47wqneqgq98ysol622totrvbjqhovp1bh4gw3l29bz&ep=v1_gifs_search&rid=200w.webp&ct=g"
-      />
+      <img alt="stock" src="200w.webp" />
     </div>
   );
 }
